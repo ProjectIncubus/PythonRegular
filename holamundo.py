3e356020b2885b11e3f20c9af9bc35755d5345d3
Gustavo Adulfo López Ramírez <134171934+ProjectIncubus@users.noreply.github.com>
2023-06-27 20:48:00 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,11 @@
+# Este es mi primer codigo
+"""
+Este es un comentario
+de multiples lineas
+:)
+"""
+
+print('Hola, Python!')
+print("Esta es otra forma de imprimir en la terminal.")
+
+print(type('Hola, Python'))
